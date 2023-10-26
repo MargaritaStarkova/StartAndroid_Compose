24427a918a86b2e552c2b725a5e7f6adbad29061
Margarita Starkova <margaritabespa@gmail.com>
2023-10-26 09:45:48 +0300

    [Compose] lesson 1

---
+++ app/src/main/java/com/example/compose/HomeScreen.kt
@@ -0,0 +1,10 @@
+package com.example.compose
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    Text(text = "Home Screen")
+    Text(text = "123456789")
+}
