e62c2a94716601e58bbf2ab0915a9ec7f8802cc7
Margarita Starkova <margaritabespa@gmail.com>
2023-10-26 09:58:54 +0300

    [Compose] lesson 2

--- app/src/main/java/com/example/compose/HomeScreen.kt
+++ app/src/main/java/com/example/compose/HomeScreen.kt
@@ -2,9 +2,21 @@ package com.example.compose
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeScreen() {
-    Text(text = "Home Screen")
-    Text(text = "123456789")
+    Text(
+        text = "Home Screen",
+        fontSize = 22.sp,
+        color = Color.Green,
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen()
 }
