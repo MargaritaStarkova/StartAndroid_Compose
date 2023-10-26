786bc43e8670afb9115db65bc73188dada249f40
Margarita Starkova <margaritabespa@gmail.com>
2023-10-26 12:50:41 +0300

    [Compose] Lesson 4. Row, Column, Box

--- app/src/main/java/com/example/compose/HomeScreen.kt
+++ app/src/main/java/com/example/compose/HomeScreen.kt
@@ -1,30 +1,99 @@
 package com.example.compose
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeScreen() {
-    Text(
-        text = "Home screen",
-        fontSize = 32.sp,
-        color = Color.White,
-        maxLines = 1,
-        overflow = TextOverflow.Ellipsis,
-        textAlign = TextAlign.Center,
-        modifier = Modifier
-            .background(color = Color.Black)
-            .fillMaxWidth(),
+    /*Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Bottom,
+    ) {
+        *//*Text(
+            text = "Home screen",
+            fontSize = 32.sp,
+            color = Color.White,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .background(color = Color.Black)
+                .fillMaxWidth(),
 
-    )
+            )*//*
+        Text(
+            text = "Title",
+            color = Color.Black,
+            fontSize = 32.sp
+        )
+        Text(
+            text = "Description",
+            color = Color.Black,
+            fontSize = 22.sp,
+            modifier = Modifier.align(Alignment.Start)
+        )
+    }*/
+
+    /*    Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(text = "Name", fontSize = 20.sp)
+            Spacer(modifier = Modifier.weight(1f))
+            Text(text = "Surname", fontSize = 20.sp)
+        }*/
+
+    /*Column {
+        Text(text = "N", fontSize = 48.sp,
+            modifier = Modifier.background(Color.Green))
+        Text(text = "ame",
+            modifier = Modifier.background(Color.Gray))
+    }*/
+    /*    Row(verticalAlignment = Alignment.CenterVertically) {
+            Box {
+                Text(
+                    text = "N", fontSize = 48.sp,
+                    //modifier = Modifier.background(Color.Green)
+                )
+                Text(
+                    text = "ame",
+                    modifier = Modifier
+                        // .background(Color.Gray)
+                        .align(Alignment.BottomCenter)
+                )
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Column(horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.background(Color.Green).padding(start = 32.dp, top = 16.dp)) {
+                Text(text = "Title")
+                Text(text = "Description")
+            }
+        }*/
+
+    /*if (list.isEmpty()) {
+        Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+            Text(text = "Empty screen")
+        }
+    } else {
+        Column {
+            for (s in list) {
+                Text(text = s)
+            }
+        }
+    }*/
+
+    Column(
+        modifier = Modifier.padding(start = dimensionResource(id = R.dimen.large_padding))
+    ) {
+        Text(text = stringResource(id = R.string.home_screen_title))
+        Text(text = stringResource(id = R.string.home_screen_description))
+    }
+
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
