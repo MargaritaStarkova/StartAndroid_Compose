c9a347a4a0dce371b53792ffe22f6461c198b0be
Margarita Starkova <margaritabespa@gmail.com>
2023-10-26 10:07:18 +0300

    [Compose] Lesson 3. Modifier

--- app/src/main/java/com/example/compose/HomeScreen.kt
+++ app/src/main/java/com/example/compose/HomeScreen.kt
@@ -1,17 +1,29 @@
 package com.example.compose
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeScreen() {
     Text(
-        text = "Home Screen",
-        fontSize = 22.sp,
-        color = Color.Green,
+        text = "Home screen",
+        fontSize = 32.sp,
+        color = Color.White,
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .background(color = Color.Black)
+            .fillMaxWidth(),
+
     )
 }
 
