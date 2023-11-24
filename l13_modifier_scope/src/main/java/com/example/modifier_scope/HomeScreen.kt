0708863d735d642950efde34c000e3c889cae2a6
Margarita Starkova <margo.ivi@yandex.ru>
2023-11-24 11:46:46 +0300

    [Compose] Lesson 13. Scope для Modifier

---
+++ l13_modifier_scope/src/main/java/com/example/modifier_scope/HomeScreen.kt
@@ -0,0 +1,15 @@
+package com.example.modifier_scope
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomeScreen() {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Text(text = "Some text", modifier = Modifier.align(CenterHorizontally))
+    }
+}
