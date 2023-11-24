0361e851981f67db89c38479a51d4214371de661
Margarita Starkova <margo.ivi@yandex.ru>
2023-11-24 11:19:13 +0300

    [Compose] Lesson 13. Modifier как параметр Composable функции

--- l13_modifier/src/main/java/com/example/modifier/HomeScreen.kt
+++ l13_modifier/src/main/java/com/example/modifier/HomeScreen.kt
@@ -1,16 +1,17 @@
 package com.example.modifier
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -18,17 +19,17 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeScreen() {
-    Column(
-        modifier = Modifier
-            .size(200.dp)
-            .border(2.dp, Color.Red)
-            .padding(32.dp)
-            .border(2.dp, Color.Black)
-            .background(Color.Green)
-    ) {
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .padding(16.dp)) {
         Text(text = "Some text", fontSize = 30.sp)
         Spacer(modifier = Modifier.height(8.dp))
-        MyButton(text = "Button", onClick = { TODO() })
+        MyButton(
+            text = "Button", onClick = { TODO() },
+            modifier = Modifier
+                .width(100.dp)
+                .align(CenterHorizontally)
+        )
     }
 }
 
@@ -36,9 +37,10 @@ fun HomeScreen() {
 fun MyButton(
     text: String,
     onClick: () -> Unit,
+    modifier: Modifier = Modifier,
 ) {
     Text(
-        text = text, modifier = Modifier
+        text = text, modifier = modifier
             .clickable(onClick = onClick)
             .border(1.dp, Color.Black, RoundedCornerShape(4.dp))
             .padding(8.dp)
