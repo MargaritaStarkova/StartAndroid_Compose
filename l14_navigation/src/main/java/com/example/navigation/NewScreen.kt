d32fd209dc4362fa9e239dc299b6e79217ad1a67
Старкова Маргарита <mstarkova@MacBook-Pro--mstarkova.local>
2023-12-01 09:29:27 +0300

    NewScreen Test

---
+++ l14_navigation/src/main/java/com/example/navigation/NewScreen.kt
@@ -0,0 +1,21 @@
+package com.example.navigation
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun NewScreen(
+    onNavigateToOrders: () -> Unit,
+    onNavigateToUsers: () -> Unit,
+) {
+    Column {
+        Text(text = "Home screen")
+
+        Text(text = "Orders", modifier = Modifier.clickable(onClick = onNavigateToOrders))
+        Text(text = "Users", modifier = Modifier.clickable(onClick = onNavigateToUsers))
+    }
+
+}
